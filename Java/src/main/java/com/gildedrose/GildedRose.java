a83dab5784f7e7275b7e05f6bf0df7865bc9396c
Ludo <ludovic.pradel@saagie.com>
2021-07-07 16:55:07 +0200

    move decrease sellIn on top of the method

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -18,38 +18,32 @@ class GildedRose {
             return;
         }
 
+        item.sellIn = item.sellIn - 1;
+
         if (item.name.equals("Aged Brie")) {
             increaseQuality(item);
+            if (item.sellIn < 0) {
+                increaseQuality(item);
+            }
 
         } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
             increaseQuality(item);
 
-            if (item.sellIn < 11) {
+            if (item.sellIn < 10) {
                 increaseQuality(item);
             }
 
-            if (item.sellIn < 6) {
+            if (item.sellIn < 5) {
                 increaseQuality(item);
+            }
+
+            if (item.sellIn < 0) {
+                item.quality = 0;
             }
 
 
         } else {
             decreaseQuality(item);
-        }
-
-
-        item.sellIn = item.sellIn - 1;
-
-        if (item.name.equals("Aged Brie")) {
-            if (item.sellIn < 0) {
-                increaseQuality(item);
-            }
-        } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-            if (item.sellIn < 0) {
-                item.quality = 0;
-            }
-
-        } else {
             if (item.sellIn < 0) {
                 decreaseQuality(item);
             }
