a8243ee7a4c1a22877dfc2127ba8b1f80b277de9
Ludo <ludovic.pradel@saagie.com>
2021-07-07 16:26:44 +0200

    if appears now on first row

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -35,15 +35,16 @@ class GildedRose {
                 }
 
             }
+        } else if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
+            if (item.quality > 0) {
+
+            }
         } else {
             if (item.quality > 0) {
-                if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
-
-                } else {
-                    item.quality = item.quality - 1;
-                }
+                item.quality = item.quality - 1;
             }
         }
+
 
         if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
 
@@ -51,23 +52,29 @@ class GildedRose {
             item.sellIn = item.sellIn - 1;
         }
 
-        if (item.sellIn < 0) {
-            if (item.name.equals("Aged Brie")) {
+        if (item.name.equals("Aged Brie")) {
+            if (item.sellIn < 0) {
                 if (item.quality < 50) {
                     item.quality = item.quality + 1;
                 }
-            } else {
-                if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    item.quality = item.quality - item.quality;
-                } else {
-                    if (item.quality > 0) {
-                        if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                            return;
-                        }
-                        item.quality = item.quality - 1;
-                    }
+            }
+        } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            if (item.sellIn < 0) {
+                item.quality = item.quality - item.quality;
+            }
+        } else if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
+            if (item.sellIn < 0) {
+                if (item.quality > 0) {
+                    return;
+                }
+            }
+        } else {
+            if (item.sellIn < 0) {
+                if (item.quality > 0) {
+                    item.quality = item.quality - 1;
                 }
             }
         }
     }
 }
+
