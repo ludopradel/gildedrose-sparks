2bfd7566091f479dfcd1b3f66bc060ce0f3d5f37
Ludo <ludovic.pradel@saagie.com>
2021-07-07 16:34:47 +0200

    extract increase & decrease Quality

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -19,30 +19,22 @@ class GildedRose {
         }
 
         if (item.name.equals("Aged Brie")) {
-            if (item.quality < 50) {
-                item.quality = item.quality + 1;
-            }
+            increaseQuality(item);
+
         } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-            if (item.quality < 50) {
-                item.quality = item.quality + 1;
+            increaseQuality(item);
 
-                if (item.sellIn < 11) {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1;
-                    }
-                }
-
-                if (item.sellIn < 6) {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1;
-                    }
-                }
-
+            if (item.sellIn < 11) {
+                increaseQuality(item);
             }
+
+            if (item.sellIn < 6) {
+                increaseQuality(item);
+            }
+
+
         } else {
-            if (item.quality > 0) {
-                item.quality = item.quality - 1;
-            }
+            decreaseQuality(item);
         }
 
 
@@ -50,21 +42,29 @@ class GildedRose {
 
         if (item.name.equals("Aged Brie")) {
             if (item.sellIn < 0) {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
-                }
+                increaseQuality(item);
             }
         } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
             if (item.sellIn < 0) {
-                item.quality = item.quality - item.quality;
+                item.quality = 0;
             }
 
         } else {
             if (item.sellIn < 0) {
-                if (item.quality > 0) {
-                    item.quality = item.quality - 1;
-                }
+                decreaseQuality(item);
             }
+        }
+    }
+
+    private void decreaseQuality(Item item) {
+        if (item.quality > 0) {
+            item.quality = item.quality - 1;
+        }
+    }
+
+    private void increaseQuality(Item item) {
+        if (item.quality < 50) {
+            item.quality = item.quality + 1;
         }
     }
 }
