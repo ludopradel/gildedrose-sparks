e2ac27721a91529c6f563f5e5ce18644a4f0e81d
Ludo <ludovic.pradel@saagie.com>
2021-07-07 16:10:39 +0200

    adding coverage with approval

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,17 +1,28 @@
 package com.gildedrose;
 
+import org.approvaltests.combinations.CombinationApprovals;
+import org.approvaltests.legacycode.Range;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GildedRoseTest {
 
     @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+    void updateQualityForItems() {
+        String[] names = {"sword", "Aged Brie", "Sulfuras, Hand of Ragnaros", "Backstage passes to a TAFKAL80ETC concert"};
+        Integer[] sellIns = Range.get(-20, 30);
+        Integer[] qualities = Range.get(-20, 60);
+
+
+        CombinationApprovals.verifyAllCombinations(this::doUpdateQuality, names, sellIns, qualities);
+    }
+
+    private String doUpdateQuality(String name, int sellIn, int quality) {
+        Item[] items = new Item[] { new Item(name, sellIn, quality) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+
+        String item = app.items[0].toString();
+        return item;
     }
 
 }
